7497e84a02f425781342b70250ad1725ae96da7e
lashley12 <149909799+lashley12@users.noreply.github.com>
2026-03-26 11:43:40 -0500

    Add files via upload

---
+++ Week 8 - Programming Lab/CollectionLab-Lashley/frmCollections.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace CollectionLab_Lashley
+{
+    partial class frmCollections
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnStats = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.rtbCollection = new System.Windows.Forms.RichTextBox();
+            this.rtbStats = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(355, 44);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(104, 17);
+            this.lblNumber.TabIndex = 0;
+            this.lblNumber.Text = "Enter a Number:";
+            this.lblNumber.Click += new System.EventHandler(this.lblNumber_Click);
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(358, 64);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(88, 25);
+            this.txtNumber.TabIndex = 1;
+            this.txtNumber.TextChanged += new System.EventHandler(this.txtNumber_TextChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(145, 310);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(66, 24);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add Number";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnStats
+            // 
+            this.btnStats.Location = new System.Drawing.Point(275, 310);
+            this.btnStats.Name = "btnStats";
+            this.btnStats.Size = new System.Drawing.Size(66, 24);
+            this.btnStats.TabIndex = 3;
+            this.btnStats.Text = "Statistics";
+            this.btnStats.UseVisualStyleBackColor = true;
+            this.btnStats.Click += new System.EventHandler(this.btnStats_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(420, 310);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(66, 24);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(561, 310);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(66, 24);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // rtbCollection
+            // 
+            this.rtbCollection.BackColor = System.Drawing.Color.Lavender;
+            this.rtbCollection.ForeColor = System.Drawing.Color.Black;
+            this.rtbCollection.Location = new System.Drawing.Point(240, 138);
+            this.rtbCollection.Name = "rtbCollection";
+            this.rtbCollection.ReadOnly = true;
+            this.rtbCollection.Size = new System.Drawing.Size(101, 127);
+            this.rtbCollection.TabIndex = 6;
+            this.rtbCollection.Text = "";
+            this.rtbCollection.TextChanged += new System.EventHandler(this.rtbCollection_TextChanged);
+            // 
+            // rtbStats
+            // 
+            this.rtbStats.BackColor = System.Drawing.Color.MistyRose;
+            this.rtbStats.ForeColor = System.Drawing.Color.Black;
+            this.rtbStats.Location = new System.Drawing.Point(469, 149);
+            this.rtbStats.Name = "rtbStats";
+            this.rtbStats.ReadOnly = true;
+            this.rtbStats.Size = new System.Drawing.Size(158, 102);
+            this.rtbStats.TabIndex = 7;
+            this.rtbStats.Text = "";
+            this.rtbStats.TextChanged += new System.EventHandler(this.rtbStats_TextChanged);
+            // 
+            // frmCollections
+            // 
+            this.AcceptButton = this.btnAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(834, 461);
+            this.Controls.Add(this.rtbStats);
+            this.Controls.Add(this.rtbCollection);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStats);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.lblNumber);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmCollections";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Collection Lab";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnStats;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.RichTextBox rtbCollection;
+        private System.Windows.Forms.RichTextBox rtbStats;
+    }
+}
+
---
+++ Week 8 - Programming Lab/CollectionLab-Lashley/frmCollections.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+Lauren Lashley
+Object-Oriented Prg Using C# - ITD-2343
+Due: March 4, 2026
+Week 8 Programming Lab - Collection Lab
+*/
+
+namespace CollectionLab_Lashley
+{
+    public partial class frmCollections : Form
+    {
+        private const int MAX_ELEMENTS = 17;
+        private const int MAX_VALUE = 1217;
+        private const int MIN_VALUE = -1217;
+
+        private List<int> numbers = new List<int>();
+
+        public frmCollections()
+        {
+            InitializeComponent();
+        }
+
+        private bool ValidateInput(out int value)
+        {
+            value = 0;
+
+            if (string.IsNullOrWhiteSpace(txtNumber.Text))
+            {
+                rtbStats.Text = "Please enter a number.";
+                txtNumber.Text = "";
+                return false;
+            }
+
+            // makes sure only whole numbers were entered
+            if (!int.TryParse(txtNumber.Text, out value))
+            {
+                rtbStats.Text = "Only whole numbers are allowed.";
+                txtNumber.Text = "";
+                return false;
+            }
+
+            // the min value
+            if (value < MIN_VALUE)
+            {
+                rtbStats.Text = "Value is too small.";
+                txtNumber.Text = "";
+                return false;
+            }
+
+            // the max value
+            if (value > MAX_VALUE)
+            {
+                rtbStats.Text = "Value is too big.";
+                txtNumber.Text = "";
+                return false;
+            }
+
+            return true;
+        }
+
+        private void lblNumber_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNumber_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            rtbCollection.Clear();
+            rtbStats.Clear();
+            int value;
+
+            if (!ValidateInput(out value))
+                return;
+            if (numbers.Count >= MAX_ELEMENTS)
+            {
+                rtbStats.Text = "Collection is full. Maximum of 17 numbers allowed.";
+                txtNumber.Text = "";
+                return;
+            }
+
+            numbers.Add(value);
+            txtNumber.Text = "";
+        }
+
+        private void btnStats_Click(object sender, EventArgs e)
+        {
+            if (numbers.Count == 0)
+            {
+                rtbStats.Text = "Collection is empty.";
+                return;
+            }
+
+            rtbCollection.Clear();
+
+            int highest = numbers[0];
+            int lowest = numbers[0];
+            int total = 0;
+
+            foreach (int num in numbers)
+            {
+                rtbCollection.AppendText(num.ToString() + Environment.NewLine);
+
+                total += num;
+
+                if (num > highest)
+                    highest = num;
+
+                if (num < lowest)
+                    lowest = num;
+            }
+
+            double average = (double)total / numbers.Count;
+
+            rtbStats.Text =
+                "High Number: " + highest + Environment.NewLine +
+                "Low Number: " + lowest + Environment.NewLine +
+                "Average: " + average.ToString("F4") + Environment.NewLine +
+                "Count: " + numbers.Count;
+
+            numbers.Clear();
+            txtNumber.Text = "";
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            numbers.Clear();
+            rtbCollection.Clear();
+            rtbStats.Text = "Collection has been cleared.";
+            txtNumber.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+              this.Close();
+        }
+
+        private void rtbCollection_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rtbStats_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
