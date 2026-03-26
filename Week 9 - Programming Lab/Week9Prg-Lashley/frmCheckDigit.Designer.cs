9debb9fa050cf1a494179e0fac09974e93c9f148
lashley12 <149909799+lashley12@users.noreply.github.com>
2026-03-26 11:43:58 -0500

    Add files via upload

---
+++ Week 9 - Programming Lab/Week9Prg-Lashley/frmCheckDigit.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Week9Prg_Lashley
+{
+    partial class frmCheckDigit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAccount = new System.Windows.Forms.Label();
+            this.lblConfirm = new System.Windows.Forms.Label();
+            this.lblPayment = new System.Windows.Forms.Label();
+            this.txtAccount = new System.Windows.Forms.TextBox();
+            this.txtConfirm = new System.Windows.Forms.TextBox();
+            this.txtPayment = new System.Windows.Forms.TextBox();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.rtbMessage = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // lblAccount
+            // 
+            this.lblAccount.AutoSize = true;
+            this.lblAccount.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAccount.Location = new System.Drawing.Point(115, 23);
+            this.lblAccount.Name = "lblAccount";
+            this.lblAccount.Size = new System.Drawing.Size(123, 18);
+            this.lblAccount.TabIndex = 0;
+            this.lblAccount.Text = "Account Number:";
+            // 
+            // lblConfirm
+            // 
+            this.lblConfirm.AutoSize = true;
+            this.lblConfirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConfirm.Location = new System.Drawing.Point(306, 23);
+            this.lblConfirm.Name = "lblConfirm";
+            this.lblConfirm.Size = new System.Drawing.Size(180, 18);
+            this.lblConfirm.TabIndex = 1;
+            this.lblConfirm.Text = "Confirm Account Number:";
+            // 
+            // lblPayment
+            // 
+            this.lblPayment.AutoSize = true;
+            this.lblPayment.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPayment.Location = new System.Drawing.Point(573, 22);
+            this.lblPayment.Name = "lblPayment";
+            this.lblPayment.Size = new System.Drawing.Size(125, 18);
+            this.lblPayment.TabIndex = 2;
+            this.lblPayment.Text = "Payment Amount:";
+            // 
+            // txtAccount
+            // 
+            this.txtAccount.Location = new System.Drawing.Point(118, 54);
+            this.txtAccount.Name = "txtAccount";
+            this.txtAccount.Size = new System.Drawing.Size(120, 22);
+            this.txtAccount.TabIndex = 3;
+            // 
+            // txtConfirm
+            // 
+            this.txtConfirm.Location = new System.Drawing.Point(309, 54);
+            this.txtConfirm.Name = "txtConfirm";
+            this.txtConfirm.Size = new System.Drawing.Size(177, 22);
+            this.txtConfirm.TabIndex = 4;
+            // 
+            // txtPayment
+            // 
+            this.txtPayment.Font = new System.Drawing.Font("Symbol", 8.25F);
+            this.txtPayment.Location = new System.Drawing.Point(576, 54);
+            this.txtPayment.Name = "txtPayment";
+            this.txtPayment.Size = new System.Drawing.Size(122, 21);
+            this.txtPayment.TabIndex = 5;
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcess.Location = new System.Drawing.Point(160, 139);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(102, 30);
+            this.btnProcess.TabIndex = 6;
+            this.btnProcess.Text = "&Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(309, 139);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(106, 30);
+            this.btnReset.TabIndex = 7;
+            this.btnReset.Text = "&Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(450, 139);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(109, 30);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // rtbMessage
+            // 
+            this.rtbMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbMessage.BackColor = System.Drawing.Color.Beige;
+            this.rtbMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbMessage.Location = new System.Drawing.Point(66, 206);
+            this.rtbMessage.Name = "rtbMessage";
+            this.rtbMessage.ReadOnly = true;
+            this.rtbMessage.Size = new System.Drawing.Size(617, 232);
+            this.rtbMessage.TabIndex = 9;
+            this.rtbMessage.Text = "";
+            // 
+            // frmCheckDigit
+            // 
+            this.AcceptButton = this.btnProcess;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtbMessage);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.txtPayment);
+            this.Controls.Add(this.txtConfirm);
+            this.Controls.Add(this.txtAccount);
+            this.Controls.Add(this.lblPayment);
+            this.Controls.Add(this.lblConfirm);
+            this.Controls.Add(this.lblAccount);
+            this.Name = "frmCheckDigit";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Check Digit Verification";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAccount;
+        private System.Windows.Forms.Label lblConfirm;
+        private System.Windows.Forms.Label lblPayment;
+        private System.Windows.Forms.TextBox txtAccount;
+        private System.Windows.Forms.TextBox txtConfirm;
+        private System.Windows.Forms.TextBox txtPayment;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.RichTextBox rtbMessage;
+    }
+}
+
---
+++ Week 9 - Programming Lab/Week9Prg-Lashley/frmCheckDigit.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+/*
+Lauren Lashley
+Object-Oriented Prg Using C# - ITD 2343
+Week 9 Programming Lab
+Due: March 8, 2026
+*/
+
+namespace Week9Prg_Lashley
+{
+    public partial class frmCheckDigit : Form
+    {
+        private bool ValidateInputs(string account, string confirm,
+                            string paymentInput,
+                            out decimal payment)
+        {
+            payment = 0;
+
+            if (string.IsNullOrWhiteSpace(account) ||
+                string.IsNullOrWhiteSpace(confirm) ||
+                string.IsNullOrWhiteSpace(paymentInput))
+            {
+                rtbMessage.Text = "All fields are required.";
+                return false;
+            }
+
+            if (!account.All(char.IsDigit) ||
+                !confirm.All(char.IsDigit))
+            {
+                rtbMessage.Text = "Account numbers must be numeric.";
+                return false;
+            }
+
+            if (account.Length != 8 || confirm.Length != 8)
+            {
+                rtbMessage.Text = "Account number must be exactly 8 digits.";
+                return false;
+            }
+
+            if (account != confirm)
+            {
+                rtbMessage.Text = "Account numbers do not match.";
+                return false;
+            }
+
+            if (!decimal.TryParse(paymentInput,
+                NumberStyles.Currency,
+                CultureInfo.CurrentCulture,
+                out payment))
+            {
+                rtbMessage.Text = "Invalid payment amount.";
+                return false;
+            }
+            return true;
+        }
+
+        private bool IsValidCheckDigit(string account)
+        {
+            int total = 0;
+
+            for (int i = 0; i < account.Length - 1; i++)
+            {
+                total += int.Parse(account[i].ToString());
+            }
+
+            int checkDigit = total % 10;
+            int lastDigit = int.Parse(account[account.Length - 1].ToString());
+
+            return checkDigit == lastDigit;
+        }
+
+        private void DisplaySuccess(string account, decimal payment)
+        {
+            string date = DateTime.Now.ToString("MMMM dd, yyyy");
+
+            rtbMessage.Text =
+                $"A payment of {payment.ToString("C")} was applied to account {account} on {date}.";
+        }
+
+        public frmCheckDigit()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            rtbMessage.Text = "";
+
+            string account = txtAccount.Text.Trim();
+            string confirm = txtConfirm.Text.Trim();
+            string paymentInput = txtPayment.Text.Trim();
+
+            if (!ValidateInputs(account, confirm, paymentInput, out decimal payment))
+                return;
+
+            if (!IsValidCheckDigit(account))
+            {
+                rtbMessage.Text = "Invalid account number. Check digit does not match.";
+                return;
+            }
+
+            DisplaySuccess(account, payment);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtAccount.Text = "";
+            txtConfirm.Text = "";
+            txtPayment.Text = "";
+            rtbMessage.Text = "";
+            txtAccount.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
