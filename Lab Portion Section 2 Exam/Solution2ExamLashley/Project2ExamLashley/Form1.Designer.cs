e09632a49e24033f1032d2bce0f887254f0047c8
lashley12 <149909799+lashley12@users.noreply.github.com>
2026-03-26 11:39:51 -0500

    Add files via upload

---
+++ Lab Portion Section 2 Exam/Solution2ExamLashley/Project2ExamLashley/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Project2ExamLashley
+{
+    partial class frmOperations
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShowModulus = new System.Windows.Forms.Button();
+            this.btnShowFactorial = new System.Windows.Forms.Button();
+            this.btnShowFibonacci = new System.Windows.Forms.Button();
+            this.btnDoModulus = new System.Windows.Forms.Button();
+            this.btnDoFactorial = new System.Windows.Forms.Button();
+            this.btnDoFibonacci = new System.Windows.Forms.Button();
+            this.txtOperand1 = new System.Windows.Forms.TextBox();
+            this.txtOperand2 = new System.Windows.Forms.TextBox();
+            this.lblMessageArea = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnShowModulus
+            // 
+            this.btnShowModulus.BackColor = System.Drawing.Color.PapayaWhip;
+            this.btnShowModulus.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShowModulus.Location = new System.Drawing.Point(135, 30);
+            this.btnShowModulus.Name = "btnShowModulus";
+            this.btnShowModulus.Size = new System.Drawing.Size(216, 84);
+            this.btnShowModulus.TabIndex = 1;
+            this.btnShowModulus.Text = "Show Modulus\n";
+            this.btnShowModulus.UseVisualStyleBackColor = false;
+            this.btnShowModulus.Click += new System.EventHandler(this.btnShowModulus_Click);
+            // 
+            // btnShowFactorial
+            // 
+            this.btnShowFactorial.BackColor = System.Drawing.Color.PapayaWhip;
+            this.btnShowFactorial.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShowFactorial.Location = new System.Drawing.Point(381, 29);
+            this.btnShowFactorial.Name = "btnShowFactorial";
+            this.btnShowFactorial.Size = new System.Drawing.Size(216, 84);
+            this.btnShowFactorial.TabIndex = 3;
+            this.btnShowFactorial.Text = "Show Factorial\n";
+            this.btnShowFactorial.UseVisualStyleBackColor = false;
+            this.btnShowFactorial.Click += new System.EventHandler(this.btnShowFactorial_Click);
+            // 
+            // btnShowFibonacci
+            // 
+            this.btnShowFibonacci.BackColor = System.Drawing.Color.PapayaWhip;
+            this.btnShowFibonacci.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShowFibonacci.Location = new System.Drawing.Point(633, 30);
+            this.btnShowFibonacci.Name = "btnShowFibonacci";
+            this.btnShowFibonacci.Size = new System.Drawing.Size(216, 84);
+            this.btnShowFibonacci.TabIndex = 5;
+            this.btnShowFibonacci.Text = "Show Fibonacci";
+            this.btnShowFibonacci.UseVisualStyleBackColor = false;
+            this.btnShowFibonacci.Click += new System.EventHandler(this.btnShowFibonacci_Click);
+            // 
+            // btnDoModulus
+            // 
+            this.btnDoModulus.BackColor = System.Drawing.Color.PapayaWhip;
+            this.btnDoModulus.Enabled = false;
+            this.btnDoModulus.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDoModulus.Location = new System.Drawing.Point(135, 129);
+            this.btnDoModulus.Name = "btnDoModulus";
+            this.btnDoModulus.Size = new System.Drawing.Size(216, 84);
+            this.btnDoModulus.TabIndex = 2;
+            this.btnDoModulus.Text = "&Do Modulus\n";
+            this.btnDoModulus.UseVisualStyleBackColor = false;
+            this.btnDoModulus.Click += new System.EventHandler(this.btnDoModulus_Click);
+            // 
+            // btnDoFactorial
+            // 
+            this.btnDoFactorial.BackColor = System.Drawing.Color.PapayaWhip;
+            this.btnDoFactorial.Enabled = false;
+            this.btnDoFactorial.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDoFactorial.Location = new System.Drawing.Point(381, 129);
+            this.btnDoFactorial.Name = "btnDoFactorial";
+            this.btnDoFactorial.Size = new System.Drawing.Size(216, 84);
+            this.btnDoFactorial.TabIndex = 4;
+            this.btnDoFactorial.Text = "&Do Factorial\n";
+            this.btnDoFactorial.UseVisualStyleBackColor = false;
+            this.btnDoFactorial.Click += new System.EventHandler(this.btnDoFactorial_Click);
+            // 
+            // btnDoFibonacci
+            // 
+            this.btnDoFibonacci.BackColor = System.Drawing.Color.PapayaWhip;
+            this.btnDoFibonacci.Enabled = false;
+            this.btnDoFibonacci.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDoFibonacci.Location = new System.Drawing.Point(633, 130);
+            this.btnDoFibonacci.Name = "btnDoFibonacci";
+            this.btnDoFibonacci.Size = new System.Drawing.Size(216, 84);
+            this.btnDoFibonacci.TabIndex = 6;
+            this.btnDoFibonacci.Text = "&Do Fibonacci";
+            this.btnDoFibonacci.UseVisualStyleBackColor = false;
+            this.btnDoFibonacci.Click += new System.EventHandler(this.btnDoFibonacci_Click);
+            // 
+            // txtOperand1
+            // 
+            this.txtOperand1.Location = new System.Drawing.Point(359, 244);
+            this.txtOperand1.Name = "txtOperand1";
+            this.txtOperand1.Size = new System.Drawing.Size(161, 20);
+            this.txtOperand1.TabIndex = 6;
+            this.txtOperand1.TabStop = false;
+            this.txtOperand1.TextChanged += new System.EventHandler(this.txtOperand1_TextChanged);
+            // 
+            // txtOperand2
+            // 
+            this.txtOperand2.Location = new System.Drawing.Point(359, 270);
+            this.txtOperand2.Name = "txtOperand2";
+            this.txtOperand2.Size = new System.Drawing.Size(161, 20);
+            this.txtOperand2.TabIndex = 7;
+            this.txtOperand2.TabStop = false;
+            this.txtOperand2.Visible = false;
+            this.txtOperand2.TextChanged += new System.EventHandler(this.txtOperand2_TextChanged);
+            // 
+            // lblMessageArea
+            // 
+            this.lblMessageArea.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lblMessageArea.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMessageArea.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessageArea.Location = new System.Drawing.Point(54, 308);
+            this.lblMessageArea.Name = "lblMessageArea";
+            this.lblMessageArea.Size = new System.Drawing.Size(850, 128);
+            this.lblMessageArea.TabIndex = 8;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(599, 456);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(136, 54);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(768, 456);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(136, 54);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmOperations
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(1207, 536);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblMessageArea);
+            this.Controls.Add(this.txtOperand2);
+            this.Controls.Add(this.txtOperand1);
+            this.Controls.Add(this.btnDoFibonacci);
+            this.Controls.Add(this.btnDoFactorial);
+            this.Controls.Add(this.btnDoModulus);
+            this.Controls.Add(this.btnShowFibonacci);
+            this.Controls.Add(this.btnShowFactorial);
+            this.Controls.Add(this.btnShowModulus);
+            this.KeyPreview = true;
+            this.Name = "frmOperations";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Operations Lauren Lashley";
+            this.Load += new System.EventHandler(this.frmOperations_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShowModulus;
+        private System.Windows.Forms.Button btnShowFactorial;
+        private System.Windows.Forms.Button btnShowFibonacci;
+        private System.Windows.Forms.Button btnDoModulus;
+        private System.Windows.Forms.Button btnDoFactorial;
+        private System.Windows.Forms.Button btnDoFibonacci;
+        private System.Windows.Forms.TextBox txtOperand1;
+        private System.Windows.Forms.TextBox txtOperand2;
+        private System.Windows.Forms.Label lblMessageArea;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Lab Portion Section 2 Exam/Solution2ExamLashley/Project2ExamLashley/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+Name: Lauren Lashley
+Class: Obj-Oriented Prg with C#
+Semester: Spring 2026
+Assignment: Lab Portion Section 2 Exam
+*/
+
+namespace Project2ExamLashley
+{
+    public partial class frmOperations : Form
+    {
+        public frmOperations()
+        {
+            InitializeComponent();
+        }
+
+        private bool ValidateRange(int value, int max)
+        {
+            if (value < 0 || value > max)
+            {
+                lblMessageArea.Text += $"\nValue must be between 0 and {max}.";
+                return false;
+            }
+
+            return true;
+        }
+
+        private void btnShowModulus_Click(object sender, EventArgs e)
+        {
+            lblMessageArea.Text = "Enter two whole numbers to perform long division.";
+
+            txtOperand1.Visible = true;
+            txtOperand2.Visible = true;
+
+            btnDoModulus.Enabled = true;
+            btnDoFactorial.Enabled = false;
+            btnDoFibonacci.Enabled = false;
+        }
+
+        private void btnShowFactorial_Click(object sender, EventArgs e)
+        {
+            lblMessageArea.Text = "Enter one whole number between 0 and 15 to calculate factorial.";
+
+            txtOperand1.Visible = true;
+            txtOperand2.Visible = false;
+
+            btnDoModulus.Enabled = false;
+            btnDoFactorial.Enabled = true;
+            btnDoFibonacci.Enabled = false;
+        }
+
+        private void btnShowFibonacci_Click(object sender, EventArgs e)
+        {
+            lblMessageArea.Text = "Enter one whole number between 0 and 45 to calculate Fibonacci.";
+
+            txtOperand1.Visible = true;
+            txtOperand2.Visible = false;
+
+            btnDoModulus.Enabled = false;
+            btnDoFactorial.Enabled = false;
+            btnDoFibonacci.Enabled = true;
+        }
+
+        private void btnDoModulus_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtOperand1.Text) ||
+                    string.IsNullOrWhiteSpace(txtOperand2.Text))
+                {
+                    lblMessageArea.Text += "\nBoth operands must be entered.";
+                    return;
+                }
+
+                int dividend = int.Parse(txtOperand1.Text);
+                int divisor = int.Parse(txtOperand2.Text);
+
+                if (divisor == 0)
+                {
+                    lblMessageArea.Text += "\nCannot divide by zero.";
+                    return;
+                }
+
+                int quotient = 0;
+                int remainder = dividend;
+
+                while (remainder >= divisor)
+                {
+                    remainder -= divisor;
+                    quotient++;
+                }
+
+                lblMessageArea.Text +=
+                    $"\n{dividend} divided by {divisor} is {quotient} with a remainder of {remainder}";
+            }
+            catch (FormatException)
+            {
+                lblMessageArea.Text += "\nOnly whole numbers are allowed. Decimal values are prohibited.";
+            }
+            catch (Exception)
+            {
+                lblMessageArea.Text += "\nAn unexpected error occurred.";
+            }
+        }
+
+        private void btnDoFactorial_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtOperand1.Text))
+                {
+                    lblMessageArea.Text += "\nOperand cannot be empty.";
+                    return;
+                }
+
+                int number = int.Parse(txtOperand1.Text);
+
+                if (!ValidateRange(number, 15))
+                    return;
+
+                if (number == 0 || number == 1)
+                {
+                    lblMessageArea.Text += $"\nThe answer to {number}! is 1";
+                    return;
+                }
+
+                int f = 1;
+                int i = 1;
+
+                while (i <= number)
+                {
+                    f *= i;
+                    i++;
+                }
+
+                lblMessageArea.Text += $"\nThe answer to {number}! is {f}";
+            }
+            catch (FormatException)
+            {
+                lblMessageArea.Text += "\nOnly whole numbers are allowed. Decimal values are prohibited.";
+            }
+            catch (Exception)
+            {
+                lblMessageArea.Text += "\nAn unexpected error occurred.";
+            }
+        }
+
+        private void btnDoFibonacci_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtOperand1.Text))
+                {
+                    lblMessageArea.Text += "\nOperand cannot be empty.";
+                    return;
+                }
+
+                int number = int.Parse(txtOperand1.Text);
+
+                if (!ValidateRange(number, 45))
+                    return;
+
+                if (number == 0)
+                {
+                    lblMessageArea.Text += "\nFibonacci(0) = 0";
+                    return;
+                }
+
+                if (number == 1)
+                {
+                    lblMessageArea.Text += "\nFibonacci(1) = 1";
+                    return;
+                }
+
+                int prev = 0;
+                int current = 1;
+                int next = 0;
+
+                for (int i = 2; i <= number; i++)
+                {
+                    next = prev + current;
+                    prev = current;
+                    current = next;
+                }
+
+                lblMessageArea.Text += $"\nFibonacci({number}) = {current}";
+            }
+            catch (FormatException)
+            {
+                lblMessageArea.Text += "\nOnly whole numbers are allowed. Decimal values are prohibited.";
+            }
+            catch (Exception)
+            {
+                lblMessageArea.Text += "\nAn unexpected error occurred.";
+            }
+        }
+
+        private void txtOperand1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtOperand2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmOperations_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblMessageArea.Text = "Message area cleared. Operands remain unchanged.";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Thank you for using the Operations App!",
+                            "Goodbye");
+
+            this.Close();
+        }
+    }
+}
